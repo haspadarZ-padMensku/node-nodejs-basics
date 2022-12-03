f834250ba55eaab840c250761880fcd9f84add80
haspadarz-padmensku <gonchds@gmail.com>
2022-12-03 22:26:09 +0300

    hash: calc hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import { createHash } from 'crypto';
+import { getDirname } from '../utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('SHA256');
+    const __dirname = getDirname(import.meta.url);
+    const filesPath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+    try {
+        const input = fs.createReadStream(filesPath);
+        input.on('readable', () => {
+            const data = input.read();
+            if (data) {
+                hash.update(data);
+            } else {
+                console.log(hash.digest('hex'));
+            }
+        }); 
+    } catch (err) {
+        throw err;
+    }
 };
 
 await calculateHash();
