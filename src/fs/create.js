63d0f55018839b17ed516640918a99f0423d1b25
haspadarz-padmensku <gonchds@gmail.com>
2022-12-03 13:48:25 +0300

    fs: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import { isExist, getDirname } from '../utils.js';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = getDirname(import.meta.url);
+    const filePath = `${__dirname}/files/fresh.txt`;
+
+    try {
+        const isFileExist = await isExist(filePath);
+
+        if (isFileExist) {
+            throw new Error('FS operation failed');
+        }
+
+        await fs.appendFile(filePath, 'I am fresh and young');
+    } catch (err) {
+        throw err;
+    }
 };
 
 await create();
