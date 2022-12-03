9755f060f1624f96ca6e6ea885a26e9c4675e74b
haspadarz-padmensku <gonchds@gmail.com>
2022-12-03 14:33:39 +0300

    fs: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import { isExist, getDirname } from '../utils.js';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = getDirname(import.meta.url);
+    const filePath = `${__dirname}/files/fileToRead.txt`;
+
+    try {
+        const isFileExist = await isExist(filePath);
+
+        if (!isFileExist) {
+            throw new Error('FS operation failed');
+        }
+
+        const content = await fs.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch (err) {
+        throw err;
+    }
 };
 
 await read();
