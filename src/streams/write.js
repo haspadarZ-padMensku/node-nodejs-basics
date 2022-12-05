fe9c60a1a5867a074482dcbc67980150a929b2b3
haspadarz-padmensku <gonchds@gmail.com>
2022-12-05 10:12:11 +0300

    stream: write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import { getDirname } from '../utils.js';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = getDirname(import.meta.url);
+    const filesPath = `${__dirname}/files/fileToWrite.txt`;
+
+    try {
+        const input = fs.createWriteStream(filesPath);
+        process.stdin.on('data', (chunk) => {
+            input.write(chunk)
+        });
+    } catch (err) {
+        throw err;
+    }
 };
 
 await write();
