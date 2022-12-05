8b20a44aff20e39843541eb8772454d685b80b3c
haspadarz-padmensku <gonchds@gmail.com>
2022-12-05 11:35:25 +0300

    Zlib: decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+import zlib from 'zlib';
+import { isExist, getDirname } from '../utils.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = getDirname(import.meta.url);
+    const filePath = `${__dirname}/files/fileToCompress.txt`;
+    const compressedFilePath = `${__dirname}/files/archive.gz`;
+
+    try {
+        const isFileExist = await isExist(compressedFilePath);
+
+        if (!isFileExist) {
+            throw new Error('FS operation failed');
+        }
+
+        const source = fs.createReadStream(compressedFilePath);
+        const destination = fs.createWriteStream(filePath);
+        const gzip = zlib.createGunzip();
+        pipeline(source, gzip, destination);
+    } catch (err) {
+        throw err;
+    }
 };
 
 await decompress();
